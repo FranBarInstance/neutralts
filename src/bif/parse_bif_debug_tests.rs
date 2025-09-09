3c978f8b898693a409577e1ccdc451359ea3a5b1
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2025-09-09 16:55:49 +0200

    add modifier filter to debug tests

--- src/bif/parse_bif_debug_tests.rs
+++ src/bif/parse_bif_debug_tests.rs
@@ -61,6 +61,70 @@ mod tests {
     }
 
     #[test]
+    fn test_bif_debug_filter() {
+        let schema = r#"
+        {
+            "config": {
+                "debug_expire": 10,
+                "debug_file": "/tmp/enable-neutral-debug-8ndmdj76gals33-A"
+            },
+            "data": {
+                "test_debug": "true",
+                "code": "<div>{:;test_debug:}</div>"
+            }
+        }
+        "#
+        .trim();
+        let mut template = match crate::Template::new() {
+            Ok(tpl) => tpl,
+            Err(error) => {
+                println!("Error creating Template: {}", error);
+                assert!(false);
+                return;
+            }
+        };
+        fs::File::create("/tmp/enable-neutral-debug-8ndmdj76gals33-A").expect("Failed to create debug file for test");
+        template.merge_schema_str(SCHEMA).unwrap();
+        template.merge_schema_str(schema).unwrap();
+        template.set_src_str("<div>{:&debug; data->code :}</div>");
+        let result = template.render();
+        assert!(!template.has_error());
+        assert_eq!(result, "<div>&quot;&lt;div&gt;&#123;:;test_debug:&#125;&lt;&#x2F;div&gt;&quot;</div>");
+    }
+
+    #[test]
+    fn test_bif_debug_no_filter() {
+        let schema = r#"
+        {
+            "config": {
+                "debug_expire": 10,
+                "debug_file": "/tmp/enable-neutral-debug-8ndmdj76gals33-A"
+            },
+            "data": {
+                "test_debug": "true",
+                "code": "<div>{:;test_debug:}</div>"
+            }
+        }
+        "#
+        .trim();
+        let mut template = match crate::Template::new() {
+            Ok(tpl) => tpl,
+            Err(error) => {
+                println!("Error creating Template: {}", error);
+                assert!(false);
+                return;
+            }
+        };
+        fs::File::create("/tmp/enable-neutral-debug-8ndmdj76gals33-A").expect("Failed to create debug file for test");
+        template.merge_schema_str(SCHEMA).unwrap();
+        template.merge_schema_str(schema).unwrap();
+        template.set_src_str("<div>{:debug; data->code :}</div>");
+        let result = template.render();
+        assert!(!template.has_error());
+        assert_eq!(result, "<div>\"<div>{:;test_debug:}</div>\"</div>");
+    }
+
+    #[test]
     fn test_bif_debug_expire() {
         let schema = r#"
         {
