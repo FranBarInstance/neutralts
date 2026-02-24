59548cc4bcb39d3d7a44bfb5387f597d57cebf72
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2026-02-24 20:24:30 +0100

    fix: resolve borrow checker error in parse_bif_eval

--- src/bif/parse_bif_eval.rs
+++ src/bif/parse_bif_eval.rs
@@ -24,11 +24,12 @@ impl<'a> Bif<'a> {
         if (self.params != EMPTY_STRING) ^ self.mod_negate {
             if self.code.contains(BIF_OPEN) {
                 let restore_eval = self.get_data("__eval__");
-                self.set_data("__eval__", &self.params.clone());
+                let val = self.params.clone();
+                self.set_data("__eval__", &val);
                 self.code = new_child_parse!(self, &self.code, self.mod_scope);
                 self.set_data("__eval__", &restore_eval);
             }
-            self.out = self.code.clone();
+            self.out = self.code.to_string();
         } else {
             self.out = EMPTY_STRING;
         }
